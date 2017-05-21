cbb78313f3fdc9f3b048259ddda8dee74c62b81e
Hugo Silva <hugoflsilva@gmail.com>
2017-05-21 19:52:39 +0100

    Created a new drawcube function just so I can test the camera class that I need to build for view the models that I import

--- SDL2 Engine/Main.cpp
+++ SDL2 Engine/Main.cpp
@@ -1,33 +1,83 @@
 #include "Engine/Engine.h"
-#include "Engine/Components/Sprite.h"
+//#include "Engine/Components/Sprite.h"
 //#include "Engine/Components/Model.h"
 //#include "Engine/Components/Shader.h"
 
 #include <iostream>
 using namespace std;
 
+void drawCube() {
+	glBegin(GL_QUADS);
+
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+
+	// Bottom face (y = -1.0f)
+	glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	// Front face  (z = 1.0f)
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+
+	// Back face (z = -1.0f)
+	glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+
+	// Left face (x = -1.0f)
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	// Right face (x = 1.0f)
+	glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	glEnd();  // End of drawing color-cube
+}
+
 int main(int argc, char **argv)
-{	
+{
 	Engine engine;
 	bool validInit = engine.Initialize("Lego Demo!");
 
 	if (validInit) {
 
-		Sprite testSprite = Sprite("Assets/Textures/texture.png", Vector3(Engine::SCREEN_WIDTH / 2, Engine::SCREEN_HEIGHT / 2, 0));
-		//if (GLEW_OK != glewInit())
-		//{
-		//	std::cout << "Failed to initialize GLEW" << std::endl;
-		//	return EXIT_FAILURE;
-		//}
-
+		//Sprite testSprite = Sprite("Assets/Textures/texture.png", Vector3(Engine::SCREEN_WIDTH / 2, Engine::SCREEN_HEIGHT / 2, 0));
 		//Shader shader("Assets/Shaders/Default.vs", "Assets/Shaders/Default.frag");
 		//Model model("Assets/Models/stormtrooper.fbx");
-
+		int i = 0;
 		while (engine.GetActive()) {
+
+			Uint32 deltaTime = SDL_GetTicks();
+
 			engine.Update();
 			engine.BeginRender();
-			testSprite.Render();
+			//testSprite.Render();
 			//model.Draw(shader);
+
+			glMatrixMode(GL_MODELVIEW);
+			glLoadIdentity();                 // Reset the model-view matrix
+			glRotatef(i++, 1.0f, 1.0f, 0.0f);
+			drawCube();
+
 			engine.EndRender();
 		}
 	}
