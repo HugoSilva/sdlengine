166384259e09e800b508af0a22d7dede25389b7e
Hugo Silva <hugoflsilva@gmail.com>
2017-06-05 00:24:20 +0100

    #1 Added a new manager for IO
    While continuing to split the code in order to achieve single
    responsibility systems I have now added input to the engine.

--- SDL2 Engine/Engine/Application.cpp
+++ SDL2 Engine/Engine/Application.cpp
@@ -5,7 +5,7 @@ Application::Application()
 	window = new Window("Lego Demo");
 	scene = new Scene();
 	//create renderer
-	//control inputs
+	input = new Input();
 	running = true;
 }
 
@@ -18,7 +18,8 @@ bool Application::Run()
 	now = SDL_GetTicks();
 	deltaTime = ((float)(now - last)) / 1000;
 	last = now;
-	scene->Update();
+	this->HandleEvents();
+	scene->Update(deltaTime);
 
 	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -28,4 +29,30 @@ bool Application::Run()
 	SDL_GL_SwapWindow(window->GetWindow());
 
 	return running;
+}
+
+void Application::HandleEvents()
+{
+	if (SDL_PollEvent(&windowEvent)) {
+		switch (windowEvent.type) {
+		case SDL_QUIT:
+			this->SetRunning(false);
+			break;
+		case SDL_KEYDOWN:
+		case SDL_KEYUP:
+			input->HandleKeyboard(windowEvent.key);
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+			input->HandleMouseButton(windowEvent.button);
+		case SDL_MOUSEMOTION:
+			input->HandleMouseMotion(windowEvent.motion);
+			break;
+		}
+	}
+}
+
+void Application::SetRunning(bool value)
+{
+	running = value;
 }
--- SDL2 Engine/Engine/Application.h
+++ SDL2 Engine/Engine/Application.h
@@ -4,6 +4,7 @@
 #include <SDL.h>
 #include "Window.h"
 #include "Scene.h"
+#include "Input.h"
 
 class Application
 {
@@ -12,13 +13,17 @@ public:
 	~Application();
 
 	bool Run();
+	void HandleEvents();
+	void SetRunning(bool value);
 
 private:
 	bool running;
+	SDL_Event windowEvent;
 	long now, last = 0;
 	float deltaTime = 0.0;
 	Window* window;
 	Scene* scene;
+	Input* input;
 };
 
 #endif
