857f0ea614d0bb0aec94ad348e32cf24855a04b4
Hugo Silva <hugoflsilva@gmail.com>
2018-06-02 22:03:26 +0100

    More thread development to keep track of all existing threads

--- Engine/Engine/ThreadManager.cpp
+++ Engine/Engine/ThreadManager.cpp
@@ -1,10 +1,27 @@
 #include "ThreadManager.h"
 
-#include <thread>
 
 unsigned int ThreadManager::m_AvailableThreads;
+std::vector<std::thread*> ThreadManager::m_Threads;
 
 void ThreadManager::Init()
 {
 	m_AvailableThreads = std::thread::hardware_concurrency();
+}
+
+void ThreadManager::Start()
+{
+	for (int num = 0; num < m_AvailableThreads; num++)
+	{
+		m_Threads.push_back(new std::thread(Physics::loop, &swapper_physics));
+	}
+}
+
+void ThreadManager::Clean()
+{
+	for (int i = 0; i < m_AvailableThreads; i++)
+	{
+		m_Threads[i]->join();
+		//delete m_Threads[i];
+	}
 }
--- Engine/Engine/ThreadManager.h
+++ Engine/Engine/ThreadManager.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <vector>
+#include <thread>
+
 class ThreadManager
 {
 public:
 	static void Init();
+	static void Start();
+	static void Clean();
 
 private:
 	ThreadManager() { }
 
 	static unsigned int m_AvailableThreads;
+	static std::vector<std::thread*> m_Threads;
 };
