1eacfac32c56f6108a45786f9dac345e660ee211
Hugo Silva <hugoflsilva@gmail.com>
2018-06-06 13:09:08 +0100

    Test project configuration and made a simple soundmanager test

--- UnitTests/Main.cpp
+++ UnitTests/Main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <Audio/SoundManager.h>
 
 using namespace std;
+using namespace audio;
 
-int main(int argc, char* argv[])
+TEST(SoundManager, AddSound)
+{
+	SoundManager::add(new Sound("bgm", "Resources/background.ogg"));
+	EXPECT_EQ(1, 1);
+}
+
+TEST(SoundManager, AddMusic)
+{
+	SoundManager::add(new Music("bgm", "Resources/background.ogg"));
+	EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv)
 {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
