57e22646dec7372a2b5f4343c27355d946ad8786
Hugo Silva <hugoflsilva@gmail.com>
2018-06-12 01:23:18 +0100

    Created a different file for the gles2 implementation based on a pull request on imgui github

--- Engine/Engine/Utils/imgui_impl_sdl_gles2.h
+++ Engine/Engine/Utils/imgui_impl_sdl_gles2.h
@@ -1,15 +1,5 @@
-#pragma once
-// ImGui SDL2 binding with OpenGL (legacy, fixed pipeline)
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan graphics context creation, etc.)
-
-// Implemented features:
-//  [X] User texture binding. Cast 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-// Missing features:
-//  [ ] SDL2 handling of IME under Windows appears to be broken and it explicitly disable the regular Windows IME. You can restore Windows IME by compiling SDL with SDL_DISABLE_WINDOWS_IME.
-
-// **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
-// **Prefer using the code in the sdl_opengl3_example/ folder**
-// See imgui_impl_sdl.cpp for details.
+// ImGui SDL2 binding with OpenGL ES2
+// In this binding, ImTextureID is used to store an OpenGL 'GLuint' texture identifier. Read the FAQ about ImTextureID in imgui.cpp.
 
 // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
 // If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
