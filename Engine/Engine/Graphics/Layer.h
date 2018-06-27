8bc01bcfea87dc29a938008c37d19a17b7760f65
Hugo Silva <hugoflsilva@gmail.com>
2018-06-27 08:13:41 +0100

    Added the remove function to the layer class

--- Engine/Engine/Graphics/Layer.h
+++ Engine/Engine/Graphics/Layer.h
@@ -12,6 +12,7 @@ namespace graphics
 		Layer(Renderer2D* renderer, Shader* shader);
 		virtual ~Layer();
 		virtual void add(Renderable2D* renderable);
+		virtual void remove(Renderable2D* renderable);
 		virtual void render();
 
 		inline const std::vector<Renderable2D*>& getRenderables() const { return m_Renderables; }
