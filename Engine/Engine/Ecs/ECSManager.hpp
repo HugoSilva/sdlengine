59ebae40ccf1ed7c00e941a491684bfef57b6f26
Hugo Silva <hugoflsilva@gmail.com>
2018-08-09 08:10:55 +0100

    Started converting the old ecs to use the entt ecs system

--- Engine/Engine/Ecs/ECSManager.hpp
+++ Engine/Engine/Ecs/ECSManager.hpp
@@ -5,6 +5,8 @@
 #include "ComponentDefinitions.hpp"
 #include <vector>
 
+#include <entity/registry.hpp>
+
 namespace ecs
 {
 	class ECSManager
@@ -18,6 +20,19 @@ namespace ecs
 		static EntityHandle addEntity(TArgs... arguments)
 		{
 			return m_ecs.makeEntity((arguments)...);
+
+			auto entity = registry.create();
+			registry.destroy(entity);
+		}
+
+		uint32_t createEntity()
+		{
+			return registry.create();
+		}
+
+		void destroyEntity(uint32_t entity)
+		{
+			registry.destroy(entity);
 		}
 
 		static void addSystem(SystemBase& system);
@@ -32,5 +47,7 @@ namespace ecs
 	private:
 		static std::vector<SystemList*> m_SystemLists;
 		static ECS m_ecs;
+		
+		entt::DefaultRegistry registry;
 	};
 }
